3e69124ea9d010aa64ef11fdeacfc6ef1eee6f6b
Takahiro Takahashi <takachan88@gmail.com>
2019-08-06 14:37:18 +0900

    New File
    New One

---
+++ code1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+const char *message = "Hello";
+char password[32];
+char buffer[256];
+ 
+memcpy(buffer, message, 256);
+
+
+}
