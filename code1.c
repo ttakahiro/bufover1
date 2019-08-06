0ae8b85c09954fbd457c66715e3ebabd8207b140
Takahiro Takahashi <takahiro.takahashi@accelsofteng.com>
2019-08-07 02:41:27 +0900

    back to 256

--- code1.c
+++ code1.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUF_SIZE  256
-
 int main(int argc, char *argv[]){
 
 	const char *message = "Hello";
 	char password[32];
-	char buffer[BUF_SIZE];
+	char buffer[256];
  
-	memcpy(buffer, message, BUF_SIZE);
+	memcpy(buffer, message, 256);
 
 
 }
